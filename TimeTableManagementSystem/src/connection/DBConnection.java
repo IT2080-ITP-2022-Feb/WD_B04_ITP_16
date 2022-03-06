1db7fa056958b80226f06c91ac086c6aac2f5c33
IT19212050 <it19212050@my.sliit.lk>
2022-03-06 18:49:04 +0530

    Windows Builder Setting up and Initialize the Design implementation for Lec

--- TimeTableManagementSystem/src/connection/DBConnection.java
+++ TimeTableManagementSystem/src/connection/DBConnection.java
@@ -13,7 +13,7 @@ public class DBConnection {
 			try {
 				
 				Class.forName("org.sqlite.JDBC");
-				conn = DriverManager.getConnection("jdbc:sqlite:ITPM.db");
+				conn = DriverManager.getConnection("jdbc:sqlite:ITP.db");
 				System.out.println("Connected");
 			}
 			catch(Exception e) {
